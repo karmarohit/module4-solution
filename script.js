fb0d75d2f5522bd0652073279417eacf2e3cea7e
Rohit Vishwakarma <81515248+karmarohit@users.noreply.github.com>
2021-08-30 10:10:12 +0530

    save
    changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 //
 // Module 4 Assignment Instructions.
 //
-// The idea of this assignment is to take an existing array of names
+// The idea of this assignment is hhto take an existing array of names
 // and then output either Hello 'Name' or Good Bye 'Name' to the console.
 // The program should say "Hello" to any name except names that start with a "J"
 // or "j", otherwise, the program should say "Good Bye". So, the final output
